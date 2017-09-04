b0026050187ac6cf5987952d6e1e5f94ec4cf8e5
James Oliver <jamesoliver@protonmail.com>
2017-09-04 08:18:36 -0400

    basic Observable instantiation using basic array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/observable';
+
+import 'rxjs/add/observable/from';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    doRx();
+  }
+}
+
+function doRx() {
+  const numbers = [1, 5, 10];
+
+  const source = Observable.from(numbers);
+
+
 }
